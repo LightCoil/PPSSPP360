25363fbc23a20f9245e89a511762ae1e2fa9ddc0
Rapture <95989103+LightCoil@users.noreply.github.com>
2025-06-13 16:34:11 +0300

    Delete hle directory

--- hle/hle_eboot_loader.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-// Загрузить EBOOT.PBP из ISO, распаковать, загрузить ELF внутрь PSP памяти
-bool HLE_LoadEboot(const std::string &isoPath);
--- hle/hle_kernel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "cpu_state.h"
-
-// Реализация HLE‐ядра: функции псевдо‐ядра PSP (например, sceKernel* )
-namespace HLE {
-    void InitKernel(CPUState &cpu);
-    void ShutdownKernel();
-    uint32_t SyscallHandler(CPUState &cpu);
-};
