7c0af27fa081cdcd4b731d71413f253b269d4e7c
Rapture <95989103+LightCoil@users.noreply.github.com>
2025-06-13 16:34:19 +0300

    Delete jit directory

--- jit/jit_interrupt_bridge.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../core/cpu_state.h"
-
-namespace JIT {
-
-// Эта функция вызывается из JIT-блока при SYSCALL/BREAK/прерывании
-extern "C" void JitSignalException(CPUState *cpu);
-
-} // namespace JIT
