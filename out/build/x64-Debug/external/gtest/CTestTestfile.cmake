a16261fb4494f0f867b22d596734874ba25cee8c
Rapture <95989103+LightCoil@users.noreply.github.com>
2025-06-13 16:42:18 +0300

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/external/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: I:/tools/PSP360/external/gtest
-# Build directory: I:/tools/PSP360/out/build/x64-Debug/external/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
